5e2c13e7849a2780a14a4e2e447ab0584725e59e
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2022-10-11 21:37:57 +0530

    Add files via upload

---
+++ HieratchialInheritance.java
@@ -0,0 +1,27 @@
+class Animal{
+    void eat(){
+        System.out.println("eating..");
+    }
+
+    class Dog extends Animal{
+        void bark(){
+            System.out.println("barking.."); 
+        }
+    }
+    class Cat extends Animal{
+        void meow(){
+            System.out.println("meow..");
+        }
+    }
+    class TestInheritance3{
+        public static void main(String[]args){
+            Cat c = new Cat();
+            c.meow();
+            c.eat();
+            //c.bark();  it will show error
+        }
+    }
+
+} 
+    
+
