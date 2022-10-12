06fe19ca890c715a05a46bf685a55f4860d58c4e
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2022-10-12 21:28:29 +0530

    Add files via upload

---
+++ Multilevelinheritance.java
@@ -0,0 +1,23 @@
+ class Animal {
+    void eat(){
+        System.out.println("eating..");
+    }
+    class Dog extends Animal {
+        void bark(){
+            System.out.println("barking..");
+        }
+    }
+ class Babydog extends Dog{
+    void weep(){
+        System.out.println("weeping..");
+    }
+ }
+ class InheritanceTest2{
+    public static void main(String[]args){
+        Babydog d = new Babydog();
+        d.weep();
+        d.bark();
+        d.eat();
+    }
+ }
+}
